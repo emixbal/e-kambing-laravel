08e01b98ff75b81d16fabceb31a57e06987b8c78
iqbal <emixbal@gmail.com>
2022-10-31 16:17:33 +0700

    add kambing migrations

---
+++ src/database/migrations/2022_10_31_160854_create_kambings_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kambings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('number')->unique();
+            $table->boolean('is_active');
+            $table->date('birth_date');
+            $table->string('image');
+            $table->string('sex');
+            $table->unsignedBigInteger('kandang_id');
+            $table->foreign('kandang_id')->references('id')->on('kandangs');
+            $table->unsignedBigInteger('kambing_type_id');
+            $table->foreign('kambing_type_id')->references('id')->on('kambing_types');
+            $table->unsignedBigInteger('blood_type_id');
+            $table->foreign('blood_type_id')->references('id')->on('blood_types');
+            $table->unsignedBigInteger('induk_jantan_id');
+            $table->foreign('induk_jantan_id')->references('id')->on('kambings');
+            $table->unsignedBigInteger('induk_betina_id');
+            $table->foreign('induk_betina_id')->references('id')->on('kambings');
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kambings');
+    }
+};
